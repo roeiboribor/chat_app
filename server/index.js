6f9c0de4cbb7197ea3de7cf5728f48debefae98c
Roei Boribor <roeiboribor@gmail.com>
2024-02-27 17:01:28 +0800

    add express feature - incomplete

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from 'path';
 
-const SERVER = createServer();
-const PORT = 3500;
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const SERVER = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
 
 const io = new Server(SERVER, {
+    // THIS IS USE IF THE SERVER DOMAIN IS ON DIFFERENT HOSTING
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : [
-            "http://localhost:5500",
-            "http://127.0.0.1:5500"
-        ]
+        origin: process.env.NODE_ENV === "production"
+            ? false
+            : [
+                "http://localhost:5500",
+                "http://127.0.0.1:5500"
+            ]
     }
 });
 
@@ -21,5 +31,3 @@ io.on('connection', socket => {
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     });
 });
-
-SERVER.listen(PORT, () => console.log(`listening on port ${PORT}`));
