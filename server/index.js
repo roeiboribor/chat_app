72d45f52092fdaf7da1f14bd46de74fea64fef5a
Roei Boribor <roeiboribor@gmail.com>
2024-02-27 14:24:14 +0800

    add index.jsx and fix package.json

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const ws = require('ws');
+const server = new ws.server({ port: 3000 });
+server.on('connection', socket => {
+    socket.on('message', message => {
+        console.log(message);
+        socket.send(`${message}`);
+    });
+});
