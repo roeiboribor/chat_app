e6730403f6fc404796d35616f81209c503505727
Roei Boribor <roeiboribor@gmail.com>
2024-03-01 16:59:03 +0800

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,53 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import io from 'socket.io-client';
+// const socket = io.connect('http://localhost:3001');
+const socket = io.connect('http://192.168.1.87:3001');
 
 function App() {
+	const [message, setMessage] = useState('');
+	const [receivedMessage, setReceivedMessage] = useState('');
+
+	useEffect(() => {
+		receiveMessage();
+	}, []);
+
+	const submit = (e) => {
+		e.preventDefault();
+		socket.emit('send_message', message);
+		setMessage('');
+	};
+	// https://www.youtube.com/watch?v=djMy4QsPWiI
+	const receiveMessage = () => {
+		socket.on('receive_message', (data) => {
+			setReceivedMessage(data);
+		});
+	};
+
 	return (
 		<>
-			<div className="min-h-vh">
-				<div className="container">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
-					asperiores!
+			<div className="min-vh-100 bg-light">
+				<div className="container py-5">
+					<Form onSubmit={submit}>
+						<Form.Group className="mb-3" controlId="message-form">
+							<Form.Control
+								// as="textarea"
+								// rows="5"
+								type="text"
+								placeholder="Your message here..."
+								onChange={(e) => setMessage(e.target.value)}
+								value={message}
+								// style={{ resize: 'none' }}
+							/>
+						</Form.Group>
+						<Button variant="primary" type="submit">
+							Send Message
+						</Button>
+					</Form>
+					<div className="mt-5">{receivedMessage}</div>
 				</div>
 			</div>
 		</>
--- server/index.js
+++ server/index.js
@@ -10,10 +10,19 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [
-            "http://localhost:5173/"
+        origins: [
+            "http://localhost:5173",
+            "http://192.168.1.87",
         ]
     }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User is connected! ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit('receive_message', data);
+    });
 });
 
 server.listen(PORT, () => {
